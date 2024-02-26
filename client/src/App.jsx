375f45d781d566f1da35413bca70373ede40b3cc
hakankaraahmet <karaahmethkn@gmail.com>
2024-02-26 14:57:22 +0100

    frontend-project started

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "antd";
+function App() {
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+      <h1 className="primary underline">
+      Title</h1>
+    </div>
+  );
+}
+
+export default App;
